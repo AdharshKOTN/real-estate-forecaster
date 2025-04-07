80576d4ec425ebcb7c130bab742babf19c173dfd
Adharsh Rajendran <adharsh.rajendran@gmail.com>
2025-04-06 18:00:01 -0700

    updates on dependencies, CORS on server

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from app.routes.forecasting import forecast_bp
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, origins=["http://localhost:3000"])
 
     # Register Blueprints
     app.register_blueprint(forecast_bp)
